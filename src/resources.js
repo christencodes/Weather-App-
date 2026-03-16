832a88a960475510de91923864c4a051e1a5e089
christenally <13616443+Christenally@users.noreply.github.com>
2026-03-16 18:39:36 -0500

    updated resources

--- src/resources.js
+++ src/resources.js
@@ -1,40 +1,49 @@
+import sunny from "/images/icon-sunny.webp";
+import partlyCloudy from "/images/icon-partly-cloudy.webp";
+import overcast from "/images/icon-overcast.webp";
+import fog from "/images/icon-fog.webp";
+import drizzle from "/images/icon-drizzle.webp";
+import rain from "/images/icon-rain.webp";
+import snow from "/images/icon-snow.webp";
+import storm from "/images/icon-storm.webp";
+
 export const resources = [
-  { icon: "/images/icon-sunny.webp", codes: [0, 1], weather: "sunny" },
+  { icon: sunny, codes: [0, 1], weather: "sunny" },
 
   {
-    icon: "/images/icon-partly-cloudy.webp",
+    icon: partlyCloudy,
     codes: [2],
     weather: "partlyCloudy",
   },
 
   {
-    icon: "/images/icon-overcast.webp",
+    icon: overcast,
     codes: [3],
     weather: "overcast",
   },
 
-  { icon: "/images/icon-fog.webp", codes: [45, 48], weather: "fog" },
+  { icon: fog, codes: [45, 48], weather: "fog" },
 
   {
-    icon: "/images/icon-drizzle.webp",
+    icon: drizzle,
     codes: [51, 53, 55, 56, 57],
     weather: "drizzle",
   },
 
   {
-    icon: "/images/icon-rain.webp",
+    icon: rain,
     codes: [61, 63, 65, 66, 67, 80, 81, 82],
     weather: "rain",
   },
 
   {
-    icon: "/images/icon-snow.webp",
+    icon: snow,
     codes: [71, 73, 75, 77, 85, 86],
     weather: "snow",
   },
 
   {
-    icon: "/images/icon-storm.webp",
+    icon: storm,
     codes: [95, 96, 99],
     weather: "snow",
   },
